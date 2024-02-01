a737d6cf7940290159a31b778d99a3f059c37c97
S-A-Robinson <scott.robinson4477@gmail.com>
2024-02-01 17:20:02 +0000

    broke everything for a se

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ function App() {
 
   return (
     <div className="app">
-      <ul className='nav-list' data-text='HOME'>
-        <li className='nav-item' data-text='HOME'>
-          HOME
-        </li>
-        <li className='nav-item' data-text='ABOUT'>
-          ABOUT
-        </li>
-        <li className='nav-item' data-text='PORTFOLIO'>
-          PORTFOLIO
-        </li>
-        <li className='nav-item' data-text='CONTACT'>
-          CONTACT
-        </li>
-      </ul>
-      {/* <div className='disc'></div> */}
+      <div className='page-container'>
+        <div className='nav-list-container'>
+          <ul className='nav-list'>
+            <li className='nav-item-container'>
+              <div className='nav-item'>
+                <a href='localhost:3000' data-text='HOME'>HOME</a>
+              </div>
+            </li>
+            <li className='nav-item-container'>
+              <div className='nav-item'>
+                <a href='localhost:3000' data-text='ABOUT'>ABOUT</a>
+              </div>
+            </li>
+            <li className='nav-item-container'>
+              <div className='nav-item'>
+                <a href='localhost:3000' data-text='PORTFOLIO'>PORTFOLIO</a>
+              </div>
+            </li>
+            <li className='nav-item-container'>
+              <div className='nav-item'>
+                <a href='localhost:3000' data-text='CONTACT'>CONTACT</a>
+              </div>
+            </li>
+          </ul>
+          {/* <div className='disc'></div> */}
+          {/* <div className='disc-tray'></div> */}
+        </div>
+      </div>
     </div>
   );
 }
